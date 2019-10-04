6dc30493cf8d4dd5f4f1d0eae45334bbeea4d494
cuihaiyan <haiyan.cui@zebra-c.com>
2019-10-04 19:23:38 +0800

    创建订单服务

--- src/main/java/com/imooc/order/repository/OrderDetailRepository.java
+++ src/main/java/com/imooc/order/repository/OrderDetailRepository.java
@@ -1,8 +1,11 @@
 package com.imooc.order.repository;
 
+import com.imooc.order.dataobject.OrderDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 /**
  * @Author cuihaiyan
  * @Create_Time 2019-10-04 18:30
  */
-public interface OrderDetailRepository {
+public interface OrderDetailRepository extends JpaRepository<OrderDetail,String> {
 }
--- src/test/java/com/imooc/order/repository/OrderDetailRepositoryTest.java
+++ src/test/java/com/imooc/order/repository/OrderDetailRepositoryTest.java
@@ -1,6 +1,14 @@
 package com.imooc.order.repository;
 
 import com.imooc.order.OrderApplication;
+import com.imooc.order.OrderApplicationTests;
+import com.imooc.order.dataobject.OrderDetail;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
 
 import static org.junit.Assert.*;
 
@@ -8,7 +16,24 @@ import static org.junit.Assert.*;
  * @Author cuihaiyan
  * @Create_Time 2019-10-04 18:33
  */
-public class OrderDetailRepositoryTest {
+@Component
+public class OrderDetailRepositoryTest extends OrderApplicationTests {
+    @Autowired
+    private OrderDetailRepository orderDetailRepository;
 
+    @Test
+    public void testSave() {
+        OrderDetail orderDetail = new OrderDetail();
+        orderDetail.setDetailId("123456");
+        orderDetail.setOrderId("1234567890");
+        orderDetail.setProductIcon("http://xxxx.com");
+        orderDetail.setProductId("157875196366160022");
+        orderDetail.setProductName("皮蛋粥");
+        orderDetail.setProductPrice(new BigDecimal(0.01));
+        orderDetail.setProductQuantity(2);
+
+        OrderDetail result = orderDetailRepository.save(orderDetail);
+        Assert.assertTrue(result != null);
+    }
 
 }
